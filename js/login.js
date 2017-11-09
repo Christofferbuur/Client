871550b46dd28d50d49aea938eda42b46102a4e1
christofferbuur <chbu16ab@student.cbs.dk>
2017-11-09 20:46:21 +0100

    html

--- js/login.js
+++ js/login.js
@@ -6,7 +6,8 @@ $(document).ready(() => {
 //.click er et clickevent der er sat på login_button når der sker et event
     $("#login_button").click(() => {
 
-        console.log("clicked");
+            console.log("clicked");
+
 
         /*const username = $("#inputUsername").val();
         const password = $("#inputPassword").val();
--- js/sdk.js
+++ js/sdk.js
@@ -1,3 +1,53 @@
-loadNav: (cb) =>
-{
-}
+const SDK = {
+  //forbindelse til databasen
+    serverURL: "http://localhost:8080/api",
+    request: (options, cb) => {
+
+        let headers = {};
+        if (options.headers) {
+            Object.keys(options.headers).forEach((h) => {
+                headers[h] = (typeof options.headers[h] === 'object') ? JSON.stringify(options.headers[h]) : options.headers[h];
+            });
+        }
+
+        $.ajax({
+            url: SDK.serverURL + options.url,
+            method: options.method,
+            headers: headers,
+            contentType: "application/json",
+            dataType: "json",
+            data: JSON.stringify(options.data),
+            success: (data, status, xhr) => {
+                cb(null, data, status, xhr);
+            },
+            error: (xhr, status, errorThrown) => {
+                cb({xhr: xhr, status: status, error: errorThrown});
+            }
+        });
+
+
+
+    login: (email, password, cb) => {
+      //  SDK.request({
+            console.log("Kalder funktion sdk")
+
+        }
+           data: {
+                username: username,
+                password: password
+            },
+
+            url: "/users/login?include=user",
+            method: "POST"
+        }, (err, data) => {
+
+            //On login-error
+            if (err) return cb(err);
+
+            SDK.Storage.persist("tokenId", data.id);
+            SDK.Storage.persist("userId", data.userId);
+            SDK.Storage.persist("user", data.user);
+
+            cb(null, data);
+
+        });
